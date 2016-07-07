5039a7eef9c9bb35161c9aa2848006a216144747
Baz <bk@thinkloop.com>
2016-07-07 19:03:34 +0000

    generalized webserver to work with any type of file

--- webserver.js
+++ webserver.js
@@ -7,6 +7,7 @@ var nodeStatic = require("node-static"),
 	https = require("https"),
 	chalk = require("chalk"),
 	getopt = require("posix-getopt"),
+	execFile = require('child_process').execFile,
 	ssl = {
 		key: fs.readFileSync(__dirname + "/key.pem"),
 		cert: fs.readFileSync(__dirname + "/cert.pem")
@@ -15,8 +16,11 @@ var nodeStatic = require("node-static"),
 	opt,
 	directory,
 	port,
-	protocol;
+	protocol,
+	files,
+	indexFile;
 
+// parse passed-in cli options
 while ((opt = parser.getopt()) !== undefined) {
 	switch (opt.option) {
 	case 'd':
@@ -31,50 +35,63 @@ while ((opt = parser.getopt()) !== undefined) {
 	}
 }
 
-var file = new(nodeStatic.Server)(directory, {cache: 600}),
-	files = fs.readdirSync(directory),
-	htmlFile;
+// set param defaults if opts not passed in
+protocol = protocol || "http";
+port = port || process.env.PORT || 8080;
 
-for(var i in files) {
-	if(/index.*html/.test(files[i])) {
-		htmlFile = files[i];
-	}
-}
+// create index of file paths to see if they match later
+execFile('find', [directory, ' -type f '], function(doNotHandleThisErr, stdout, stderr) {
+	files =
+		stdout.split('\n')
+		.filter(function(path) {
+			return path.length && path !== directory;
+		})
+		.reduce(function(p, path) {
+			var cleanPath = path.replace(directory, '/').replace(/[/]+/, '/');
 
-runserver(protocol || "http", port || process.env.PORT || 8080);
+			// find main index file
+			if(/index.*html/.test(cleanPath)) {
+				indexFile = cleanPath;
+			}
 
-function runserver(protocol, port) {
+			p[cleanPath] = true;
+
+			return p;
+		}, {});
+
+	startServer();
+});
+
+function startServer() {
+	var staticServer = new(nodeStatic.Server)(directory, {cache: 600});
+
 	if (protocol === "https") {
 		https.createServer(ssl, serveHTTP).listen(port);
-	} else {
+	}
+	else {
 		http.createServer(serveHTTP).listen(port);
 	}
+
 	log(protocol + "://localhost:" + port.toString());
-}
 
-function serveHTTP(req, res) {
+	function serveHTTP(req, res) {
 
-	// redirect */blog/* urls to the augur.org/blog archive
-	if (req.url.match(/\/blog\//)) {
-		res.writeHead(302, {"Location": "http://www.augur.org" + req.url});
-		return res.end();
-	}
-
-    // static URIs
-	var re = /\/(styles\.css|splash\.css|images|fonts|build\.js|augur\.min\.js)/;
-
-    // route to app if not static URI
-	if (!req.url.match(re)) req.url = "/" + htmlFile;
-
-	file.serve(req, res, function (err, result) {
-		if (err) {
-			console.error("Error serving %s: %s", req.url, err.message);
-			res.writeHead(err.status, err.headers);
-			res.end();
-		} else {
-			log(req.url);
+		// if the file doesn't exist, use the default index file
+		if (!files[req.url]) {
+			req.url = indexFile;
 		}
-	});
+
+		staticServer.serve(req, res, function (err, result) {
+			if (err) {
+				console.error("Error serving %s: %s", req.url, err.message);
+				res.writeHead(err.status, err.headers);
+				res.end();
+			}
+			else {
+				log(req.url);
+			}
+		});
+	}
 }
 
 function log(str) {
